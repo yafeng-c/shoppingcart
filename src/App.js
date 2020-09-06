7f1838e8c476f5cc34fb00e85f3607fee8e15685
Y Chen <yafeng_chen@outlook.com>
2020-09-06 18:25:17 +0200

    Signed-off-by: Y Chen

--- src/App.js
+++ src/App.js
@@ -1,26 +1,114 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import data from "./data.json";
+import Products from "./components/Products";
+import Filter from "./components/Filter";
+import Cart from "./components/Cart";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      products: data.products,
+      cartItems: localStorage.getItem("cartItems")
+        ? JSON.parse(localStorage.getItem("cartItems"))
+        : [],
+      size: "",
+      sort: "",
+    };
+  }
+  createOrder = (order) => {};
+  removeFromCart = (item) => {
+    const cartItems = this.state.cartItems.slice();
+    this.setState({
+      cartItems: cartItems.filter((x) => x._id !== item._id),
+    });
+  };
+  addToCart = (product) => {
+    const cartItems = this.state.cartItems.slice();
+    let alreadyInCart = false;
+    cartItems.forEach((item) => {
+      if (item._id === product._id) {
+        item.count++;
+        alreadyInCart = true;
+      }
+    });
+    if (!alreadyInCart) {
+      cartItems.push({ ...product, count: 1 });
+    }
+    this.setState({
+      cartItems: cartItems,
+    });
+  };
+
+  componentDidUpdate() {
+    localStorage.setItem("cartItems", JSON.stringify(this.state.cartItems));
+  }
+
+  sortProducts = (event) => {
+    this.setState({
+      sort: event.target.value,
+      products: this.state.products.slice().sort((a, b) => {
+        switch (event.target.value) {
+          case "lowest":
+            return a.price - b.price;
+          case "highest":
+            return b.price - a.price;
+          case "latest":
+            return a._id - b._id;
+          default:
+            return;
+        }
+      }),
+    });
+  };
+
+  filterProducts = (event) => {
+    if (event.target.value === "") {
+      this.setState({ size: event.target.value, products: data.products });
+    } else {
+      this.setState({
+        size: event.target.value,
+        products: data.products.filter(
+          (product) => product.availableSizes.indexOf(event.target.value) >= 0
+        ),
+      });
+    }
+  };
+
+  render() {
+    return (
+      <div className="grid-container">
+        <header>
+          <a href="/"> React Shopping Cart</a>
+        </header>
+        <main>
+          <div className="content">
+            <div className="main">
+              <Filter
+                count={this.state.products.length}
+                size={this.state.size}
+                sort={this.state.sort}
+                filterProducts={this.filterProducts}
+                sortProducts={this.sortProducts}
+              />
+              <Products
+                products={this.state.products}
+                addToCart={this.addToCart}
+              />
+            </div>
+            <div className="sidebar">
+              <Cart
+                cartItems={this.state.cartItems}
+                removeFromCart={this.removeFromCart}
+                createOrder={this.createOrder}
+              />
+            </div>
+          </div>
+        </main>
+        <footer>All rights are reserved.</footer>
+      </div>
+    );
+  }
 }
 
 export default App;
